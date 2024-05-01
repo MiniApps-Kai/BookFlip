956be13917a890eb8fe6dea0352b75a4ef29ef54
Aoitoridayo <aoitoridayo1110@icloud.com>
2024-05-01 22:56:26 +0900

    Initial Commit

---
+++ BookFlip/BookFlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookFlipApp.swift
+//  BookFlip
+//
+//  Created by 渡邊魁優 on 2024/05/01.
+//
+
+import SwiftUI
+
+@main
+struct BookFlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookFlip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BookFlip
+//
+//  Created by 渡邊魁優 on 2024/05/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
