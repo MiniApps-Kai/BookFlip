01f575a6ff7c91371ada2e76b74e83dd314b5146
Aoitoridayo <aoitoridayo1110@icloud.com>
2024-05-01 23:44:26 +0900

    commit

--- BookFlip/ContentView.swift
+++ BookFlip/ContentView.swift
@@ -8,14 +8,97 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var progress: CGFloat = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                OpenableBookFlipView(config: .init(progress: progress)) { size in
+                    FrontView(size: size)
+                } insideLeft: { size in
+                    LeftView(size: size)
+                } insideRight: { size in
+                    RightView(size: size)
+                }
+                Slider(value: $progress)
+                    .padding(10)
+                    .background(.background, in: .rect(cornerRadius: 10))
+                    .padding(.top)
+            }
+            .padding()
+            .navigationTitle("Book Flip")
         }
-        .padding()
+    }
+    @ViewBuilder func FrontView(size: CGSize) -> some View {
+        Rectangle()
+            .frame(width: size.width/2, height: size.height/2)
+            .offset(y: 10) //上を削る
+    }
+    @ViewBuilder func LeftView(size: CGSize) -> some View {
+        Rectangle()
+            .frame(width: size.width/2, height: size.height/2)
+            .foregroundStyle(Color.blue)
+    }
+    @ViewBuilder func RightView(size: CGSize) -> some View {
+        Rectangle()
+            .frame(width: size.width/2, height: size.height/2)
+            .foregroundStyle(Color.red)
+    }
+}
+
+struct OpenableBookFlipView<Front: View, InsideLeft: View, InsideRight: View>: View {
+    var config: Config = .init()
+    @ViewBuilder var front: (CGSize) -> Front
+    @ViewBuilder var insideLeft: (CGSize) -> InsideLeft
+    @ViewBuilder var insideRight: (CGSize) -> InsideRight
+    var body: some View {
+        GeometryReader {
+            let size = $0.size
+            let progress = max(min(config.progress, 1), 0)
+            let rotation = progress * -100
+            let cornerRadius = config.cornerRadius
+            let shadowColor = config.shadowColor
+            
+            ZStack {
+                insideRight(size)
+                    .frame(width: size.width, height: size.height)
+                    .clipShape(
+                        .rect(
+                            topLeadingRadius: 0,
+                            bottomLeadingRadius: 0,
+                            bottomTrailingRadius: cornerRadius,
+                            topTrailingRadius: cornerRadius
+                        )
+                    )
+                    .shadow(color: shadowColor, radius: 5, x: 0, y: 0)
+    
+                front(size)
+                    .frame(width: size.width/2, height: size.height/2)
+                    .clipShape(
+                        .rect(
+                            topLeadingRadius: 0,
+                            bottomLeadingRadius: 0,
+                            bottomTrailingRadius: cornerRadius,
+                            topTrailingRadius: cornerRadius
+                        )
+                    )
+                    .rotation3DEffect(
+                        .init(degrees: rotation),
+                        //回転する方向設定
+                        axis: (x: 0, y: 1, z: 0),
+                        //支点が左側になる
+                        anchor: .leading,
+                        perspective: 0.3
+                    )
+            }
+        }
+    }
+    
+    struct Config {
+        var width: CGFloat = 150
+        var height: CGFloat = 200
+        var progress: CGFloat = 0
+        var cornerRadius: CGFloat = 10
+        var shadowColor: Color = .black
     }
 }
 
